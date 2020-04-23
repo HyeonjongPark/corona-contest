faf0fa822060ff235fa897ba7b175c42bf11b7bb
HyeonjongPark <guswhd9844@gmail.com>
2020-04-23 13:51:46 +0900

    crawl

--- 크롤링.R
+++ 크롤링.R
@@ -27,7 +27,7 @@ btn = remDr$findElement(using = "xpath" , value = '//*[@id="sub-main"]/div[4]/di
 btn$clickElement() # 검색
 
 btn2 = remDr$findElement(using = "xpath" , value = '//*[@id="datagridTab"]/a/span')
-btn2$clickElement() # 3년 지정
+btn2$clickElement() # 
 
 #btn3 = remDr$findElement(using = "xpath" , value = '/html/body/div[3]/div/div[2]/div[2]/form/input')
 #btn3$clickElement() # 재검색
@@ -36,44 +36,38 @@ frontPage = remDr$getPageSource()
 #frontPage[[1]]
 
 
-df = data.frame(NO = rep(NA,10), data_name = rep(NA,10) , admin = rep(NA,10), url = rep(NA,10), info = rep(NA,10), 
+df = data.frame(NO = rep(NA,10), categ = rep(NA,10), data_name = rep(NA,10) , admin = rep(NA,10), url = rep(NA,10), info = rep(NA,10), 
                 shape = rep(NA,10), extenstion = rep(NA,10), real_time = rep(NA,10),
                 col_name = rep(NA,10), col_sample = rep(NA,10), col_info = rep(NA,10))
 
-df
-
-total_contents = read_html(frontPage[[1]]) %>% html_nodes('.data-title') %>% html_text()
-
-total_contents = gsub("\n","",total_contents)
-total_contents = gsub("\t","",total_contents)
-total_contents = gsub("조회수","",total_contents)
-total_contents = gsub("[0-9]","",total_contents)
-total_contents = str_split(total_contents, ",")
-
-
-
-ext = read_html(frontPage[[1]]) %>% html_nodes('.data-types') %>% html_text()
-ext = str_split(ext,"\n") 
-ext[[1]] = ext[[1]][-c(1,2,3)]
-ext[[1]]
-
-ext2 = ext[[1]][nchar(ext[[1]]) > 52]
-paste(gsub(" ","",ext2), collapse = "/")
-
-
-
-12 %% 2
-
-paste(ext[[1]])
-
-ext[[]]
-gsub(" ","",ext[1])
-
 basic_url = "https://www.data.go.kr"
+
+
+
+
 
 for(i in 1:length(total_contents)) {
   df$NO[i] = i
-  df$data_name[i] = gsub(" ","",total_contents[[i]][1])
+  
+  # category
+  categ = read_html(frontPage[[1]]) %>% html_nodes('.category-filter.visible-desktop') %>% html_text()
+  categ = gsub("\n","", categ)
+  categ = gsub(" ", "", categ)
+  
+  df$categ = categ
+  
+  
+  # data name
+  
+  
+  total_contents = read_html(frontPage[[1]]) %>% html_nodes('.data-title') %>% html_text()
+  total_contents = gsub("\n","",total_contents)
+  total_contents = gsub("\t","",total_contents)
+  total_contents = gsub("조회수","",total_contents)
+  total_contents = gsub("[0-9]","",total_contents)
+  total_contents = str_split(total_contents, "                                                                                                                ")
+  df$data_name[i] = total_contents[[i]][2]
+  df$data_name[i] = gsub(" ","",df$data_name[i])
 
   adm = read_html(frontPage[[1]]) %>% html_nodes('.data-meta') %>% html_text()
   adm = str_split(adm, "\n")
@@ -85,6 +79,12 @@ for(i in 1:length(total_contents)) {
   df$url[i] = url = gsub("<a href=\"", "", url)
   df$url[i] = url = gsub("\">", "", url)
   df$url[i] = paste0(basic_url, df$url[i])
+  
+  #info
+  info = read_html(frontPage[[1]]) %>% html_nodes('.data-desc') %>% html_text()
+  info = gsub("\n                                                    ","", info)
+  df$info[i] = substr(info[i], 1, str_locate(info[i], "[.] "))
+
   
   # 제공방식 형태
   shape = read_html(frontPage[[1]]) %>% html_nodes('.data-meta') %>% html_text()
@@ -101,28 +101,81 @@ for(i in 1:length(total_contents)) {
   df$extenstion[i] = paste(gsub(" ","",ext2), collapse = "/")
   
   
-  # 페이지 이동 세부 정보
-  
-  btn = remDr$findElement(using = "xpath" , value = paste0('//*[@id="standard-list-wrapper"]/div[', i ,']/div[1]/a'))
-  btn$clickElement() # 검색
-  
-  
+  # # 페이지 이동 세부 정보
+  # 
+  # btn_to_go = remDr$findElement(using = "xpath" , value = paste0('//*[@id="standard-list-wrapper"]/div[', i ,']/div[1]/a'))
+  # btn_to_go$clickElement() # 검색
+  # 
+  # frontPage2 = remDr$getPageSource()
+  # 
+  # for(j in 1:100) {
+  #   a = read_html(frontPage2[[1]]) %>% html_node(paste0('.sbgrid_cell.sbgrid_cell_border_st.sbgrid_cell_st.sbgrid_fixed_cell_st.sbgrid_datagrid1_fixedcol_col_',j,'_style.sbgrid_ellipsis.sbgrid_common')) %>% html_text()
+  #   b = read_html(frontPage2[[1]]) %>% html_node(paste0('.sbgrid_cell.sbgrid_cell_border_st.sbgrid_cell_st.sbgrid_data_cell_st.sbgrid_ellipsis.sbgrid_datagrid1_col_col_',j,'_style.sbgrid_common')) %>% html_text()
+  #   if(!is.na(a)) {
+  #     new_list[[1]]$coln[j] = a
+  #     new_list[[1]]$colv[j] = b
+  #   }
+  #   else break
+  # }
+  # 
+  # btn_to_back = remDr$findElement(using = "xpath" , value = '//*[@id="sub-main"]/div[4]/div/div[2]/div[6]/a')
+  # btn_to_back$clickElement() # 
+  # 
+  print(i)
 }
+
+df
+
+
+#write.csv(df, "C:/Users/onycom/Desktop/workspace/프로젝트/[공간정보수집]/collect1_10.csv")
+
+
+
+
+
+
+
 btn = remDr$findElement(using = "xpath" , value = paste0('//*[@id="standard-list-wrapper"]/div[',1,']/div[1]/a'))
 btn$clickElement()
 
 
-aa = read_html(frontPage[[1]]) %>% html_nodes('.sbgrid_cell.sbgrid_cell_border_st.sbgrid_cell_st.sbgrid_fixed_cell_st.sbgrid_datagrid1_fixedcol_col_1_style.sbgrid_ellipsis.sbgrid_common') %>% html_text()
+frontPage2 = remDr$getPageSource()
 
+col_name = read_html(frontPage2[[1]]) %>% html_node(paste0('.sbgrid_cell.sbgrid_cell_border_st.sbgrid_cell_st.sbgrid_fixed_cell_st.sbgrid_datagrid1_fixedcol_col_',i,'_style.sbgrid_ellipsis.sbgrid_common')) %>% html_text()
 
+read_html(frontPage2[[1]]) %>% html_node(paste0('.sbgrid_cell.sbgrid_cell_border_st.sbgrid_cell_st.sbgrid_fixed_cell_st.sbgrid_datagrid1_fixedcol_col_',16,'_style.sbgrid_ellipsis.sbgrid_common')) %>% html_text()
 
-for(i in 1:length(total_contents)) {
+new_list = list(data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)),
+                data.frame(coln = rep(NA,100), colv = rep(NA,100)))
+
+for(i in 1:10) {
+  for(j in 1:100) {
+    a = read_html(frontPage2[[1]]) %>% html_node(paste0('.sbgrid_cell.sbgrid_cell_border_st.sbgrid_cell_st.sbgrid_fixed_cell_st.sbgrid_datagrid1_fixedcol_col_',j,'_style.sbgrid_ellipsis.sbgrid_common')) %>% html_text()
+    b = read_html(frontPage2[[1]]) %>% html_node(paste0('.sbgrid_cell.sbgrid_cell_border_st.sbgrid_cell_st.sbgrid_data_cell_st.sbgrid_ellipsis.sbgrid_datagrid1_col_col_',j,'_style.sbgrid_common')) %>% html_text()
+    if(!is.na(a)) {
+      new_list[[1]]$coln[j] = a
+      new_list[[1]]$colv[j] = b
+    }
+    else break
+  }
+  
+  btn_to_back = remDr$findElement(using = "xpath" , value = '//*[@id="sub-main"]/div[4]/div/div[2]/div[6]/a')
+  btn_to_back$clickElement() # 
   
 }
 
+new_list[[1]]
+read_html(frontPage2[[1]]) %>% html_node(paste0('.sbgrid_cell sbgrid_cell_border_st sbgrid_cell_st sbgrid_data_cell_st sbgrid_ellipsis sbgrid_datagrid1_col_col_',1,'_style sbgrid_common')) %>% html_text()
 
-paste0(total_contents[1])
-
+read_html(frontPage2[[1]]) %>% html_node(paste0('.sbgrid_cell.sbgrid_cell_border_st.sbgrid_cell_st.sbgrid_data_cell_st.sbgrid_ellipsis.sbgrid_datagrid1_col_col_1_style.sbgrid_common')) %>% html_text()
 
 
 total_contents = trimws(gsub("건 검색\\)","",total_contents))
